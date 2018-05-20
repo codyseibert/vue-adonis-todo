f77519aec0a1a584ccb92e7846d0fe85992c5200
Cody Seibert <cseibert113@gmail.com>
2018-05-20 15:11:56 -0400

    edit and delete projects

--- client/src/components/Projects.vue
+++ client/src/components/Projects.vue
@@ -1,17 +1,52 @@
 <template>
   <Panel title="Projects">
     <div
+      class="project mt-2"
       v-for="project in projects"
       :key="project.id"
     >
-      {{project.title}}
+      <v-layout row wrap>
+        <v-flex xs9 class="text-xs-left">
+          <span
+            v-if="!project.isEditMode">
+            {{project.title}}
+          </span>
+          <v-text-field
+            autofocus
+            v-if="project.isEditMode"
+            :value="project.title"
+            @keyup.enter="saveProject(project)"
+            @input="setProjectTitle({
+              project,
+              title: $event,
+            })"
+          ></v-text-field>
+        </v-flex>
+        <v-flex xs3>
+          <v-icon
+            v-if="!project.isEditMode"
+            @click="setEditMode(project)">
+            edit
+          </v-icon>
+          <v-icon
+            v-if="project.isEditMode"
+            @click="saveProject(project)">
+            check
+          </v-icon>
+          <v-icon
+            @click="deleteProject(project)">
+            delete
+          </v-icon>
+        </v-flex>
+      </v-layout>
     </div>
-    <v-layout row wrap>
+    <v-layout row wrap class="mt-4">
       <v-flex xs8>
         <v-text-field
           placeholder="My project name..."
           @input="setNewProjectName"
           :value="newProjectName"
+          @keyup.enter="createProject"
         ></v-text-field>
       </v-flex>
       <v-flex xs4>
@@ -32,6 +67,9 @@
 import { mapActions, mapMutations, mapState } from 'vuex';
 
 export default {
+  mounted() {
+    this.fetchProjects();
+  },
   computed: {
     ...mapState('projects', [
       'newProjectName',
@@ -41,14 +79,29 @@ export default {
   methods: {
     ...mapMutations('projects', [
       'setNewProjectName',
+      'setEditMode',
+      'setProjectTitle',
     ]),
     ...mapActions('projects', [
       'createProject',
+      'fetchProjects',
+      'saveProject',
+      'deleteProject',
     ]),
   },
 };
 </script>
 
 <style>
+.project {
+  font-size: 24px;
+}
 
+.icon {
+  cursor: pointer;
+}
+
+.icon:hover {
+  color: #333;
+}
 </style>
--- client/src/store/projects.js
+++ client/src/store/projects.js
@@ -1,4 +1,5 @@
 // import router from '../router';
+import Vue from 'vue';
 import HTTP from '../http';
 
 export default {
@@ -8,6 +9,24 @@ export default {
     newProjectName: null,
   },
   actions: {
+    saveProject({ commit }, project) {
+      return HTTP().patch(`projects/${project.id}`, project)
+        .then(() => {
+          commit('unsetEditMode', project);
+        });
+    },
+    deleteProject({ commit }, project) {
+      return HTTP().delete(`projects/${project.id}`)
+        .then(() => {
+          commit('removeProject', project);
+        });
+    },
+    fetchProjects({ commit }) {
+      return HTTP().get('/projects')
+        .then(({ data }) => {
+          commit('setProjects', data);
+        });
+    },
     createProject({ commit, state }) {
       return HTTP().post('/projects', {
         title: state.newProjectName,
@@ -26,6 +45,21 @@ export default {
     },
     appendProject(state, project) {
       state.projects.push(project);
+    },
+    setProjects(state, projects) {
+      state.projects = projects;
+    },
+    setProjectTitle(state, { project, title }) {
+      project.title = title;
+    },
+    setEditMode(state, project) {
+      Vue.set(project, 'isEditMode', true);
+    },
+    unsetEditMode(state, project) {
+      Vue.set(project, 'isEditMode', false);
+    },
+    removeProject(state, project) {
+      state.projects.splice(state.projects.indexOf(project), 1);
     },
   },
 };
